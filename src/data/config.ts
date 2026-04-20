dabfb184ce2fef2e9f849862a0e9c3496c67e091
HuzHacker123 <Huzefakazi3131@gmail.com>
2026-04-20 17:26:17 +0530

    intial commit

---
+++ src/data/config.ts
@@ -0,0 +1,45 @@
+const config = {
+  title: "Huzefa Qazi | AI Engineer & Full-Stack Developer",
+  description: {
+    long: "Explore the portfolio of Huzefa,projects. AI Engineer and Full-Stack Developer passionate about building intelligent, scalable, and user-focused digital experiences. I combine the power of AI with full-stack development to create seamless applications that are both smart and efficient. Whether it’s crafting dynamic web platforms or developing AI-driven solutions, I focus on delivering clean, high-performance systems that make a real impact. Let's build something amazing together!",
+    short:
+      "Discover the portfolio of Huzefa, AI Engineer & Full-Stack Developer building intelligent, scalable, and user-focused digital experiences.",
+  },
+  keywords: [
+    "Huzefa",
+    "portfolio",
+    "full-stack developer",
+    "creative technologist",
+    "web development",
+    "3D animations",
+    "interactive websites",
+    "Coding Ducks",
+    "The Booking Desk",
+    "Ghostchat",
+    "web design",
+    "GSAP",
+    "React",
+    "Next.js",
+    "Spline",
+    "Framer Motion",
+  ],
+  author: "Huzefa Qazi",
+  email: "Huzefakazi3131@gmail.com",
+  site: "https://nareshkhatri.site",
+
+  // for github stars button
+  githubUsername: "HuzHacker123",
+  githubRepo: "Portfolio",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/Huzefa__Kazi",
+    linkedin: "https://www.linkedin.com/in/huzefa-kazi/",
+    instagram: "https://www.instagram.com/huzefa__kazi/",
+    facebook: "https://www.facebook.com/huzefa.kazi.10",
+    github: "https://github.com/HuzHacker123",
+  },
+};
+export { config };
